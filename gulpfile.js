6d8b98041d8ff2d0a35266cf18ea8bc4f114e4f0
Kundan Singh <kundan.singh979@gmail.com>
2016-10-07 12:18:58 +0530

    Added eslint to project

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,34 @@
+/*eslint-env node */
+
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
-
-gulp.task('default', function() {
-    console.log('gulp running successfully');
-    gulp.watch('sass/**/*.scss', ['styles']);
-});
-
-gulp.task('styles', function() {
-    gulp.src('sass/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions']
-        }))
-        .pipe(gulp.dest('./css'));
-});
-
+var eslint = require('gulp-eslint');
 var browserSync = require('browser-sync').create();
- browserSync.init({
-     server: "./"
- });
- browserSync.stream();
+
+gulp.task('default', ['styles', 'lint'], function () {
+	console.log('gulp running successfully');
+	gulp.watch('sass/**/*.scss', ['styles']);
+	gulp.watch('js/**/*.js', ['lint']);
+
+	browserSync.init({
+		server: './'
+	});
+});
+
+gulp.task('styles', function () {
+	gulp.src('sass/**/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer({
+			browsers: ['last 2 versions']
+		}))
+		.pipe(gulp.dest('./css'));
+});
+
+gulp.task('lint', function () {
+	return gulp.src(['**/*.js', '!node_modules/**'])
+		.pipe(eslint())
+		.pipe(eslint.format())
+		.pipe(eslint.failAfterError());
+});
+browserSync.stream();
